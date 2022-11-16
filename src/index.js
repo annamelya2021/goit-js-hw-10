512482beab4ca25eff98f164fba474843d31344b
annamelya2021 <anna.basyuk@gmail.com>
2022-11-16 17:24:35 +0200

    +

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// // import './css/styles.css';
+// // import seachCountry from '../src/fetchCountries.js';
+import { fetchCountries } from './fetchCountries';
+import { refs } from './refs';
+import { renderCountryList, renderCountryInfo } from './markup';
+
+// console.log(fetchCountries);
+import debounce from 'lodash.debounce'; //
+console.log(debounce);
+import { Notify } from 'notiflix';
+console.log(Notify);
+
+const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY));
+
+function onCountryInput(e) {
+  const name = e.target.value.trim();
+  console.log(name);
+  if (name === '') {
+    return (refs.ul.innerHTML = '') && (refs.div.innerHTML = '');
+  }
+
+  fetchCountries(name)
+    .then(countries => {
+      refs.ul.innerHTML = '';
+      refs.div.innerHTML = '';
+      if (countries.length === 1) {
+        refs.ul.insertAdjacentHTML('beforeend', renderCountryList(countries));
+        refs.div.insertAdjacentHTML('beforeend', renderCountryInfo(countries));
+      } else if (countries.length >= 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else {
+        refs.ul.insertAdjacentHTML('beforeend', renderCountryList(countries));
+      }
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+    });
+}
